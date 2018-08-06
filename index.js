6ad162abb039920a39214e244a1637c06739bb68
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-08-06 07:20:33 +0530

    Fucked Up Shit Will Chnage oNce i Wake Up

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-var search=require("./searchVideo");
-search({APIKey:"AIzaSyASsTAnJAqiFiRTdC-TxjWQu6sjG0dwcZw",query:'Nucleya',maxResults:20},function(response){
+var axios = require("axios");
+var searchUrl = "https://www.googleapis.com/youtube/v3/search";
+const queryFunction = function (params) {
+     const part= 'snippet';
+    const type= 'video'
+    
+    this.key = params.APIKey;
+
+    this.q = params.query;
+    this.maxResults = params.maxResults;
+    var params=new Object;
+    params[q]=
+    console.log("Hello")
+
+
+}
+queryFunction.prototype = {
+    search: function (params, callback) {
+
+        axios.get(searchUrl, {
+            params: {
+                
+            }
+        }).then(
+            function (response) {
+                if (callback) {
+                    callback(response.data.items)
+                }
+            }
+        ).catch(function (error) {
+            console.log(error)
+        })
+
+    },
+
+    comments() {}
+}
+
+
+module.exports = queryFunction
+
+
+var newSerach = new queryFunction({
+    APIKey: "AIzaSyASsTAnJAqiFiRTdC-TxjWQu6sjG0dwcZw",
+    query: 'Nucleya',
+    maxResults: 20
+});
+newSerach.search({
+ 
+  
+}, function (response) {
     console.log(response)
 })
+//queryFunction.search(respone=>{console.log(respone)})
