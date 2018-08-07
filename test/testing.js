66770e22f64d87b850fed0f8d91adddc80b12ed2
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-08-08 03:33:23 +0530

    Fucked Shit Up

--- test/testing.js
+++ test/testing.js
@@ -1,17 +1,15 @@
-var queryFunction = require('../features/searchVideo')
-var init=require("../index");
+var queryFunction = require('../features/searchChannel')
+var init = require("../index");
 var newSerach = new queryFunction({
         key: "AIzaSyASsTAnJAqiFiRTdC-TxjWQu6sjG0dwcZw",
         maxResponse: 4,
         query: "blasterjaxx",
 
-    },
-    callback = (response) => {
-        console.log(response.data.items)
+    }, response => {
+        console.log(response)
     }
 
 );
 
 
-newSerach.request()
-
+newSerach.searchResponse()
