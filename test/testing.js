7c5c968a7200a4ce7ae5c162c0b9d9367bbe193e
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-08-08 03:35:57 +0530

    Fucked Shit Up

--- test/testing.js
+++ test/testing.js
@@ -1,5 +1,21 @@
-var queryFunction = require('../features/searchChannel')
+var queryFunction = require('../features/searchChannel');
+var newQueryFunction=require("../features/searchVideo");
 var init = require("../index");
+var serach=new newQueryFunction(
+    {
+        key: "AIzaSyASsTAnJAqiFiRTdC-TxjWQu6sjG0dwcZw",
+        maxResponse: 4,
+        query: "blasterjaxx",
+
+    }, response => {
+        console.log(response)
+    }
+)
+serach.request()
+//this Works
+
+
+//tis Does Not
 var newSerach = new queryFunction({
         key: "AIzaSyASsTAnJAqiFiRTdC-TxjWQu6sjG0dwcZw",
         maxResponse: 4,
